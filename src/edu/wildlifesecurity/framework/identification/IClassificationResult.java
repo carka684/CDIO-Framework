c1b495c805d313734dd3b4838ba9b969418a8226
Tobias Norlund <tobias.norlund@tele2.se>
2014-09-26 17:00:32 +0200

    Lagt till lite i Identification-gränssnittet

---
+++ src/edu/wildlifesecurity/framework/identification/IClassificationResult.java
@@ -0,0 +1,5 @@
+package edu.wildlifesecurity.framework.identification;
+
+public interface IClassificationResult {
+
+}
--- src/edu/wildlifesecurity/framework/identification/IIdentification.java
+++ src/edu/wildlifesecurity/framework/identification/IIdentification.java
@@ -1,7 +1,13 @@
 package edu.wildlifesecurity.framework.identification;
 
+import org.opencv.core.Mat;
+
 import edu.wildlifesecurity.framework.IComponent;
 
 public interface IIdentification extends IComponent {
 
+	Mat extractFeatures(Mat inputImage);
+	
+	IClassificationResult classify(Mat features);
+	
 }

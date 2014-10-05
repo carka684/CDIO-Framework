4bee02d63b8851e886164402c3d6b08064f61f7b
Tobias Norlund <tobias.norlund@tele2.se>
2014-10-05 22:12:35 +0200

    Test i MyClass

--- src/edu/wildlifesecurity/framework/MyClass.java
+++ src/edu/wildlifesecurity/framework/MyClass.java
@@ -1,4 +1,8 @@
 package edu.wildlifesecurity.framework;
+import java.awt.BorderLayout;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
 import java.util.List;
 
 import javax.swing.JFrame;
@@ -11,38 +15,29 @@ import ij.process.ImageProcessor;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.highgui.VideoCapture;
+import org.opencv.video.BackgroundSubtractorMOG;
 import org.opencv.video.Video;
 
 import de.lmu.ifi.dbs.jfeaturelib.features.LocalBinaryPatterns;
 
-
 public class MyClass {
 	public static void main(String[] args){
-		/*System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 		
 		VideoCapture vc = new VideoCapture("C:\\Users\\Tobias\\Documents\\Datasets\\En_person_inomhus\\scene_%04d.png");
 	
 		System.out.println("Is opened: " + vc.isOpened());
 		
 		// Grab & retrieve the next frame
-		Mat frame = new Mat();
-		vc.read(frame);*/
+		Mat img = new Mat();
+		vc.read(img);
 		
-		//1. Create the frame.
-		JFrame frame = new JFrame("FrameDemo");
-
-		//2. Optional: What happens when the frame closes?
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		//3. Create components and put them in the frame.
-		//...create emptyLabel...
-		//frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
-
-		//4. Size the frame.
-		frame.pack();
-
-		//5. Show it.
-		frame.setVisible(true);
+		// Create Background Subtractor
+		
+		Mat fgMask = new Mat();
+		BackgroundSubtractorMOG bgs = new BackgroundSubtractorMOG();
+		bgs.apply(img, fgMask);
 
 	}
+	
 }

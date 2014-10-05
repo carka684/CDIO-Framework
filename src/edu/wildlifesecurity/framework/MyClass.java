36785f9e7d848cfb807ef985344245e82ca43648
Tobias Norlund <tobias.norlund@tele2.se>
2014-10-05 22:12:03 +0200

    Påbörjat test i MyClass

--- src/edu/wildlifesecurity/framework/MyClass.java
+++ src/edu/wildlifesecurity/framework/MyClass.java
@@ -1,6 +1,8 @@
 package edu.wildlifesecurity.framework;
 import java.util.List;
 
+import javax.swing.JFrame;
+
 import ij.ImagePlus;
 import ij.io.Opener;
 import ij.process.ColorProcessor;
@@ -8,21 +10,39 @@ import ij.process.ImageProcessor;
 
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.highgui.VideoCapture;
+import org.opencv.video.Video;
 
 import de.lmu.ifi.dbs.jfeaturelib.features.LocalBinaryPatterns;
 
 
 public class MyClass {
 	public static void main(String[] args){
-		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		/*System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 		
-		ImagePlus imgProc = new Opener().openURL("http://rsb.info.nih.gov/ij/images/Dot_Blot.jpg");
-		LocalBinaryPatterns descriptor = new LocalBinaryPatterns();
-		descriptor.setNumberOfHistogramBins(10);
-		descriptor.run(imgProc.getProcessor());
+		VideoCapture vc = new VideoCapture("C:\\Users\\Tobias\\Documents\\Datasets\\En_person_inomhus\\scene_%04d.png");
+	
+		System.out.println("Is opened: " + vc.isOpened());
 		
-		List<double[]> features = descriptor.getFeatures();
+		// Grab & retrieve the next frame
+		Mat frame = new Mat();
+		vc.read(frame);*/
 		
-		System.out.println("Heeeeej");
+		//1. Create the frame.
+		JFrame frame = new JFrame("FrameDemo");
+
+		//2. Optional: What happens when the frame closes?
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		//3. Create components and put them in the frame.
+		//...create emptyLabel...
+		//frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+
+		//4. Size the frame.
+		frame.pack();
+
+		//5. Show it.
+		frame.setVisible(true);
+
 	}
 }

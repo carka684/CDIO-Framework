d5e6a385a193ce2928c936b1c720c2b1bfee6aa0
jonasforsner <jonfo428@student.liu.se>
2014-10-08 14:30:18 +0200

    Fixat mer med bakgrundsmodellering

--- src/edu/wildlifesecurity/framework/MyClass.java
+++ src/edu/wildlifesecurity/framework/MyClass.java
@@ -12,13 +12,15 @@ public class MyClass {
 	public static void main(String[] args){
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 		
-		VideoCapture vc = new VideoCapture("/Users/jonasforsner/Documents/TSBB11/Matlab/CalleHund/im%04d.png");
+		VideoCapture vc = new VideoCapture("/Users/jonasforsner/Documents/TSBB11/Matlab/CalleHundLong/im%04d.png");
 		 Imshow window1 = new Imshow("Background model");
+		Imshow window2 = new Imshow("Filtered background model");
 		System.out.println("Is opened: " + vc.isOpened());
 		BackgroundSubtractorMOG2 bgs = new BackgroundSubtractorMOG2(0, 60, true);
-		bgs.setInt("nmixtures", 10);
+		bgs.setInt("nmixtures", 3);
+		bgs.setDouble("backgroundRatio", 0.9);
 		
-		for(int frameNr = 0; frameNr < 1937; frameNr++)
+		for(int frameNr = 0; frameNr < 2776; frameNr++)
 		{
 			// Grab & retrieve the next frame
 			Mat img = new Mat();
@@ -28,35 +30,43 @@ public class MyClass {
 			
 			Mat fgMask = new Mat();
 			//bgs.setInt(name, value);
-			if(frameNr < 120)
+			if(frameNr < 1000)
 			{
-				bgs.apply(img, fgMask, 0.1);
+				bgs.apply(img, fgMask, 0.01);
 			}
 			else
 			{
-				bgs.apply(img, fgMask, 0.0001);
+				bgs.apply(img, fgMask, 0.00001);
 				
 			}
+			System.out.println(frameNr + "  ");
+			
 			
 			Mat morphKernel = new Mat();
 			morphKernel = Mat.ones(3, 3, CvType.CV_8U);
-			Imgproc.erode(fgMask, fgMask, morphKernel);
+			Mat fgMaskMod = new Mat();
+			Imgproc.erode(fgMask, fgMaskMod, morphKernel);
+			//Imgproc.erode(fgMaskMod, fgMaskMod, morphKernel);
+			//Imgproc.dilate(fgMaskMod, fgMaskMod, morphKernel); 
+			Imgproc.dilate(fgMaskMod, fgMaskMod, morphKernel);
+			
+			Imgproc.dilate(fgMaskMod, fgMaskMod, morphKernel); 
+			//Imgproc.dilate(fgMaskMod, fgMaskMod, morphKernel);
+			//Imgproc.erode(fgMaskMod, fgMaskMod, morphKernel);
+			Imgproc.erode(fgMaskMod, fgMaskMod, morphKernel);
 
 			Mat convKernel = new Mat();
 			convKernel = Mat.ones(5, 5, CvType.CV_8U);
 		
 			convKernel.mul(convKernel,0.04);
 			
-			Imgproc.filter2D(fgMask, fgMask, 0, convKernel);
+			Imgproc.filter2D(fgMaskMod, fgMaskMod, 0, convKernel);
 			int threshType = Imgproc.THRESH_BINARY;
-			Imgproc.threshold(fgMask, fgMask, 254, 255, threshType);
+			Imgproc.threshold(fgMaskMod, fgMaskMod, 254, 255, threshType);
 			
-			// Imgproc.dilate(fgMask, fgMask, kernel);
-			
-			//Imgproc.dilate(fgMask, fgMask, kernel);
-			//Imgproc.erode(fgMask, fgMask, kernel);
 			
 			window1.showImage(fgMask);
+			window2.showImage(fgMaskMod);
 		}
 	}
 }

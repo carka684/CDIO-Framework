b8ee5c93d32254faf2425fa12ca14b8fb8017a0d
carka684 <carka684@student.liu.se>
2014-10-14 14:10:31 +0200

    Added some of the HOG functions

--- src/edu/wildlifesecurity/framework/identification/IIdentification.java
+++ src/edu/wildlifesecurity/framework/identification/IIdentification.java
@@ -1,13 +1,18 @@
 package edu.wildlifesecurity.framework.identification;
 
+import java.util.Vector;
+
 import org.opencv.core.Mat;
+import org.opencv.core.Size;
+import org.opencv.ml.CvSVM;
+import org.opencv.objdetect.HOGDescriptor;
 
 import edu.wildlifesecurity.framework.IComponent;
 
 public interface IIdentification extends IComponent {
 
-	Mat extractFeatures(Mat inputImage);
-	
-	IClassificationResult classify(Mat features);
+	Mat extractHOGFeatures(Mat inputImage,HOGDescriptor hog,Size s);
+	Mat extractHOGFeaturesFromFiles(Vector<String> strVec, HOGDescriptor hog,Size s);
+	IClassificationResult classify(Mat features,CvSVM SVM);
 	
 }
--- src/edu/wildlifesecurity/framework/identification/impl/HOGIdentification.java
+++ src/edu/wildlifesecurity/framework/identification/impl/HOGIdentification.java
@@ -1,9 +1,13 @@
 package edu.wildlifesecurity.framework.identification.impl;
 
+import java.util.Vector;
+
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfFloat;
-import org.opencv.core.MatOfPoint;
 import org.opencv.core.Size;
+import org.opencv.highgui.Highgui;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.ml.CvSVM;
 import org.opencv.objdetect.HOGDescriptor;
 
 import edu.wildlifesecurity.framework.identification.IClassificationResult;
@@ -22,20 +26,35 @@ public class HOGIdentification implements IIdentification {
 	 * 
 	 */
 	@Override
-	public Mat extractFeatures(Mat inputImage) {
-		
-		HOGDescriptor descriptor = new HOGDescriptor();
+	public Mat extractHOGFeatures(Mat inputImage, HOGDescriptor hog,Size s) {
 		MatOfFloat features = new MatOfFloat();
-		MatOfPoint locations = new MatOfPoint();
-		descriptor.compute(inputImage, features, new Size(32,32), new Size(0,0), locations);
+		Imgproc.resize(inputImage, inputImage, s);
+		hog.compute(inputImage, features);			
 		
 		return features;
 	}
-
+	
 	@Override
-	public IClassificationResult classify(Mat features) {
-		// TODO Auto-generated method stub
+	public IClassificationResult classify(Mat features,CvSVM SVM) {
+		Mat results = new Mat();
+		SVM.predict_all(features,results);
 		return null;
 	}
+
+	@Override
+	public Mat extractHOGFeaturesFromFiles(Vector<String> strVec,
+			HOGDescriptor hog, Size s) 
+	{
+		Mat featMat = new Mat();
+		for(String file : strVec)
+		{
+			Mat img = Highgui.imread(file,Highgui.CV_LOAD_IMAGE_GRAYSCALE);
+			Mat feat = extractHOGFeatures(img, hog,s);
+			featMat.push_back(feat.t());
+		}
+		return featMat;
+	}
+
+	
 
 }

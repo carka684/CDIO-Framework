aa1b4fb32240bec0d545cc0b7251c2cfeeb96bf8
TobiasNorlund <tobias.norlund@tele2.se>
2014-11-06 14:59:22 +0100

    La till klass för att representera en Capture

---
+++ src/edu/wildlifesecurity/framework/tracking/Capture.java
@@ -0,0 +1,17 @@
+package edu.wildlifesecurity.framework.tracking;
+
+import java.util.Date;
+
+import org.opencv.core.Mat;
+
+public class Capture {
+
+	public Date timeStamp;
+	
+	public int trapDeviceId;
+	
+	public String position;
+	
+	public Mat image;
+	
+}

6a7c635e57ebd26a1eb4e9897260e92b75035da2
carka684 <carka684@student.liu.se>
2014-11-11 10:27:29 +0100

    Impl av tracking på plats, dock returneras inget just nu!

---
+++ src/edu/wildlifesecurity/framework/tracking/impl/KalmanTracking.java
@@ -0,0 +1,85 @@
+package edu.wildlifesecurity.framework.tracking.impl;
+
+import java.util.Iterator;
+import java.util.Vector;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+
+import com.atul.JavaOpenCV.Imshow;
+
+import edu.wildlifesecurity.framework.detection.DetectionResult;
+
+public class KalmanTracking {
+	static Vector<KalmanFilter> kalVec;
+	static int nextID;
+	static int errorDist;
+	static int numOfUnseen;
+
+	public static void init()
+	{
+		nextID = 0;
+		kalVec =  new Vector<KalmanFilter>();
+		errorDist = 80; // Read from config!!
+		numOfUnseen = 15; // -- || --
+	}
+	public void trackRegions(DetectionResult result,Mat img, Imshow show) throws Exception
+	{
+		for(Iterator<KalmanFilter> iterator = kalVec.iterator(); iterator.hasNext(); )
+		{
+			KalmanFilter kf = iterator.next();
+			kf.predict(); 
+			kf.addUnseen();
+			double[][] pos = kf.getPos();
+			Core.circle(img, new Point(pos[0][0],pos[1][0]), 5, new Scalar(255-kf.getId()*50, kf.getId()*60+80,0),5);	
+
+			if(kf.getNumOfUnseen() > numOfUnseen)
+			{
+				iterator.remove();
+				System.out.println(kf.getId() + " was removed");
+			}
+		}				
+		for(int i = 0; i < result.regions.size();i++ )
+		{
+			int x = result.regions.get(i).x + result.regions.get(i).width/2;
+			int y = result.regions.get(i).y + result.regions.get(i).height/2;
+			if(kalVec.isEmpty())
+			{
+				kalVec.add(new KalmanFilter(nextID,x,y));
+				//setRegionID = nextID++;
+				nextID++;
+			}	
+			else
+			{
+				double minError = errorDist;
+				KalmanFilter bestKalman = null;
+				for(Iterator<KalmanFilter> iterator = kalVec.iterator(); iterator.hasNext(); )
+				{
+					KalmanFilter kf = iterator.next();
+					double error = kf.getError(x,y);
+					if (error < minError)
+					{
+						minError = error;
+						bestKalman = kf;
+					}   
+				}
+				if(bestKalman != null)
+				{
+					bestKalman.seen();
+					bestKalman.correct(x, y);
+				}
+				else
+				{
+					kalVec.add(new KalmanFilter(nextID,x,y));								
+					//setRegionID = nextID;
+					nextID++;
+					break;
+				}
+			}
+		}
+		show.showImage(img);
+	}
+
+}
--- test/TrackingTest.java
+++ test/TrackingTest.java
@@ -1,14 +1,5 @@
-import jama.Matrix;
-
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Hashtable;
 import java.util.Iterator;
-import java.util.Map;
-import java.util.TreeMap;
 import java.util.Vector;
-
-import jkalman.JKalman;
 
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
@@ -22,6 +13,7 @@ import edu.wildlifesecurity.framework.detection.DetectionResult;
 import edu.wildlifesecurity.framework.detection.IDetection;
 import edu.wildlifesecurity.framework.detection.impl.DefaultDetection;
 import edu.wildlifesecurity.framework.identification.impl.ImageReader;
+import edu.wildlifesecurity.framework.tracking.impl.KalmanTracking;
 
 
 public class TrackingTest {
@@ -30,84 +22,37 @@ public class TrackingTest {
 	 * @param args
 	 * @throws Exception 
 	 */
+	static Vector<KalmanFilter> kalVec;
+	static int nextID;
+
+	public static void init()
+	{
+		nextID = 0;
+		kalVec =  new Vector<KalmanFilter>();
+	}
 	public static void main(String[] args) throws Exception {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 
 
-		Vector<KalmanFilter> kalVec = new Vector<KalmanFilter>();
 		IDetection detection = new DefaultDetection();
 		detection.init();		
-
-		boolean first = true;
+		init();
 		ImageReader reader = new ImageReader();
 		reader.readImages("C:/Users/Calle/Documents/MATLAB/images/");
 		Vector<String> files = reader.getFilesVec();
 		int k = 0;
 		Imshow show = new Imshow("");
-		int nextID = 0;
+		KalmanTracking tracker = new KalmanTracking();
+		KalmanTracking.init();
 		for(String file : files){
 			Mat img = Highgui.imread(file);
 			DetectionResult result = detection.getObjInImage(img);
 			// Ladda in bild till Mat
 			if(k++ > 2)	
 			{
-				for(Iterator<KalmanFilter> iterator = kalVec.iterator(); iterator.hasNext(); )
-				{
-					KalmanFilter kf = iterator.next();
-					kf.predict(); //Ska detta ske h�r eller efter isMatch? Testa noga!
-					kf.addUnseen();
-					double[][] pos = kf.getPos();
-					Core.circle(img, new Point(pos[0][0],pos[1][0]), 5, new Scalar(255-kf.getId()*50, kf.getId()*60+80,0),5);	
-
-					if(kf.getNumOfUnseen() > 15)
-					{
-						iterator.remove();
-						System.out.println(kf.getId() + " was removed");
-					}
-				}				
-				Thread.sleep(100);
-				for(int i = 0; i < result.regions.size();i++ )//Loopa �ver regioner
-				{
-					int x = result.regions.get(i).x + result.regions.get(i).width/2;
-					int y = result.regions.get(i).y + result.regions.get(i).height/2;
-					if(kalVec.isEmpty())
-					{
-						kalVec.add(new KalmanFilter(nextID,x,y));
-						//setRegionID = nextID++;
-						nextID++;
-					}	
-					else
-					{
-						double minError = 80;
-						KalmanFilter bestKalman = null;
-						for(Iterator<KalmanFilter> iterator = kalVec.iterator(); iterator.hasNext(); )
-						{
-							KalmanFilter kf = iterator.next();
-							double error = kf.getError(x,y);
-							if (error < minError)
-							{
-								minError = error;
-								bestKalman = kf;
-							}   
-						}
-						if(bestKalman != null)
-						{
-							bestKalman.seen();
-							bestKalman.correct(x, y);
-						}
-						else
-						{
-							kalVec.add(new KalmanFilter(nextID,x,y));								
-							//setRegionID = nextID;
-							nextID++;
-							break;
-						}
-					}
-				}
-				show.showImage(img);
+				tracker.trackRegions(result, img, show);
 			}
 		}
 	}
-
 }
 

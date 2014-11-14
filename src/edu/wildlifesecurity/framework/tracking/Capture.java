c3d69382bb546a8a02befe47509e3018758124e4
TobiasNorlund <tobias.norlund@tele2.se>
2014-11-14 16:56:52 +0100

    Fixat Actuator

--- src/edu/wildlifesecurity/framework/tracking/Capture.java
+++ src/edu/wildlifesecurity/framework/tracking/Capture.java
@@ -4,9 +4,13 @@ import java.util.Date;
 
 import org.opencv.core.Mat;
 
+import edu.wildlifesecurity.framework.identification.Classes;
+
 public class Capture {
 	
 	public int captureId;
+	
+	public Classes type;
 
 	public Date timeStamp;
 	

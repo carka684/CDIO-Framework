94f0891855f53fa2b974424bd97d031260109d47
KingeranXL <lukastallund@gmail.com>
2014-11-14 14:07:08 +0100

    Added changes to capture that uses imagepaths that are used by the GUI. should be discussed before merge to master

--- src/edu/wildlifesecurity/framework/repository/IRepository.java
+++ src/edu/wildlifesecurity/framework/repository/IRepository.java
@@ -14,18 +14,21 @@ import edu.wildlifesecurity.framework.LogEvent;
 import edu.wildlifesecurity.framework.tracking.Capture;
 
 /**
- * A repository component handles storing of data. Data such at logging and configuration.
+ * A repository component handles storing of data. Data such at logging and
+ * configuration.
  * 
  */
 public interface IRepository extends IComponent, ILogger {
-	
+
 	/**
 	 * Enables listeners to receive events when log entries are made
+	 * 
 	 * @param type
 	 * @param handler
 	 * @return
 	 */
-	ISubscription addEventHandler(EventType type, IEventHandler<LogEvent> handler);
+	ISubscription addEventHandler(EventType type,
+			IEventHandler<LogEvent> handler);
 
 	/**
 	 * Stores the given capture in the repository
@@ -33,14 +36,15 @@ public interface IRepository extends IComponent, ILogger {
 	 * @param capture
 	 */
 	void storeCapture(Capture capture);
-	
+
 	/**
-	 * Loads all captures from all trap devices from repository, excluding their images
+	 * Loads all captures from all trap devices from repository, excluding their
+	 * images
 	 * 
 	 * @return A list of captures
 	 */
 	List<Capture> getCaptureDefinitions();
-	
+
 	/**
 	 * Fetches the detection image for the given captureId from the repository
 	 * 
@@ -48,7 +52,7 @@ public interface IRepository extends IComponent, ILogger {
 	 * @return
 	 */
 	Mat getCaptureImage(Capture captureId);
-	
+
 	/**
 	 * Gets a config entry
 	 * 
@@ -66,34 +70,27 @@ public interface IRepository extends IComponent, ILogger {
 	 * @author lukas
 	 */
 	void setConfigOption(String option, Object value);
-	
-	
+
 	/**
 	 * Disposes the repository
 	 */
 	void dispose();
-	
-	/**
-	 * Writes entry to log with priority prio and message msg
-	 * @param prio
-	 * @param msg
-	 * 
-	 */
-	void log(String prio, String msg);
-	
+
 	/**
 	 * Get log entries from log that is between start time and end time
+	 * 
 	 * @param startTime
 	 * @param endTime
 	 * @author lukas
 	 */
 	public String getLog(Date startTime, Date endTime);
-	
+
 	/**
 	 * Get log entries from log that is after start time.
+	 * 
 	 * @param startTime
 	 * @author lukas
 	 */
 	public String getLog(Date startTime);
-	
+
 }
--- src/edu/wildlifesecurity/framework/tracking/Capture.java
+++ src/edu/wildlifesecurity/framework/tracking/Capture.java
@@ -1,10 +1,16 @@
 package edu.wildlifesecurity.framework.tracking;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.opencv.core.Mat;
 
 public class Capture {
+	
+	private String pathFormat= "Captures/%s/%d.png";
+	
+	private DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
 	
 	public int captureId;
 
@@ -15,5 +21,129 @@ public class Capture {
 	public String position;
 	
 	public Mat image;
+	
+	public String imagePath;
+
+	public Capture() {
+		super();
+	}
+	
+	public Capture(int captureId, Date timeStamp, int trapDeviceId,
+			String position, Mat image) {
+		super();
+		this.captureId = captureId;
+		this.timeStamp = timeStamp;
+		this.trapDeviceId = trapDeviceId;
+		this.position = position;
+		this.image = image;
+		this.imagePath = String.format(pathFormat,df.format(this.timeStamp),this.captureId);
+	}
+	
+	public Capture(int captureId, Date timeStamp, int trapDeviceId,
+			String position) {
+		super();
+		this.captureId = captureId;
+		this.timeStamp = timeStamp;
+		this.trapDeviceId = trapDeviceId;
+		this.position = position;
+		this.imagePath = String.format(pathFormat,df.format(this.timeStamp),this.captureId);
+	}
+	
+	public Capture(int captureId, Date timeStamp, int trapDeviceId,
+			String position, Mat image, String path) {
+		super();
+		this.captureId = captureId;
+		this.timeStamp = timeStamp;
+		this.trapDeviceId = trapDeviceId;
+		this.position = position;
+		this.image = image;
+		this.imagePath = path;
+	}
+
+
+	/**
+	 * @return the captureId
+	 */
+	public int getCaptureId() {
+		return captureId;
+	}
+
+	/**
+	 * @param captureId the captureId to set
+	 */
+	public void setCaptureId(int captureId) {
+		this.captureId = captureId;
+	}
+
+	/**
+	 * @return the timeStamp
+	 */
+	public Date getTimeStamp() {
+		return timeStamp;
+	}
+
+	/**
+	 * @param timeStamp the timeStamp to set
+	 */
+	public void setTimeStamp(Date timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+
+	/**
+	 * @return the trapDeviceId
+	 */
+	public int getTrapDeviceId() {
+		return trapDeviceId;
+	}
+
+	/**
+	 * @param trapDeviceId the trapDeviceId to set
+	 */
+	public void setTrapDeviceId(int trapDeviceId) {
+		this.trapDeviceId = trapDeviceId;
+	}
+
+	/**
+	 * @return the position
+	 */
+	public String getPosition() {
+		return position;
+	}
+
+	/**
+	 * @param position the position to set
+	 */
+	public void setPosition(String position) {
+		this.position = position;
+	}
+
+	/**
+	 * @return the image
+	 */
+	public Mat getImage() {
+		return image;
+	}
+
+	/**
+	 * @param image the image to set
+	 */
+	public void setImage(Mat image) {
+		this.image = image;
+	}
+	
+	/**
+	 * @return the imagePath
+	 */
+	public String getImagePath() {
+		return imagePath;
+	}
+
+	/**
+	 * @param imagePath the imagePath to set
+	 */
+	public void setImagePath(String imagePath) {
+		this.imagePath = imagePath;
+	}
+
 	
 }
